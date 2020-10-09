abcc126fd62c78e9ca111a7283a8ae2d8980301e
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2020-10-09 19:33:33 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:'CiTrNcii4IWN9FbQS8hbRGt06',
+    consumer_secret:'xrvXX2IEG3LRyYrndzOlmHFbUcJoZGxKJNb6MOKKdFpagKiBsL',
+    access_token:'80438435-8MPUmEZkV33kOe3NpqHa1ta4oRW4RdWPrJtCC0qhF',
+    access_token_secret:'pIEdSytidbR7M21Oqu7MwxlRnZAL5qAESEmYwFYXa4bMh'
+}
